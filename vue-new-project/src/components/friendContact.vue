9c50cbc66f3e2c1489d59dd8977032895b28c3b3
Lime5005 <zlp5005@gmail.com>
2020-11-01 15:51:18 +0100

    what is props

---
+++ vue-new-project/src/components/friendContact.vue
@@ -0,0 +1,47 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? 'Favorite' : ''}}</h2>
+    <button @click="toggleDetail">{{ showDetail ? 'Hide' : 'Show'}}</button>
+    <button @click="toggleFavorite">Favorite?</button>
+    <ul v-if="showDetail">
+      <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+      <li><strong>Email: </strong>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  // What defined in props are like data, can be accessed everywhere in this file. We use props to pass data from parent to child. Remember: props can not be mutated in the child, but you can bypass this role by passing it into data property (of cause, change to another name first)
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'isFavorite',
+  ],
+  data() {
+    return {
+      showDetail: false,
+      friend: 
+        {
+          id: '1',
+          name: 'lily',
+          phone: '222',
+          email: 'lily@localhost.com'
+        },
+      friendIsFavorite: this.isFavorite, 
+    }
+  },
+  methods: {
+      toggleDetail() {
+        this.showDetail = !this.showDetail;
+      },
+      toggleFavorite() {
+        if (this.friendIsFavorite === '1') {
+          this.friendIsFavorite  = '0'
+        } else {
+          this.friendIsFavorite = '1'
+        }
+      }
+  }
+}
+</script>
---
+++ vue-new-project/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+//当你在写import的时候，同时也在命名：
+import App from './App.vue'
+import friendContact from './components/friendContact.vue'
+
+const app = createApp(App)
+
+//这里先登记一下要用到的components，写component也是在给template命名：
+app.component('friend-contact', friendContact)
+app.mount('#app')
