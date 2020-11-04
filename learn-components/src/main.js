0b2f9c6eb0a0340db0371bf2d020b4ea60fa16b1
Lime5005 <zlp5005@gmail.com>
2020-11-04 19:55:19 +0100

    slot style as a global component

--- learn-components/src/main.js
+++ learn-components/src/main.js
@@ -4,9 +4,10 @@ import App from './App.vue';
 
 //保留BaseBadge在此，因为它在多处被使用，global更适合
 import BaseBadge from './components/BaseBadge.vue';
-
+import SectionStyle from './components/SectionStyle.vue';
 const app = createApp(App);
 
 app.component('base-badge', BaseBadge);
+app.component('section-style', SectionStyle);
 
 app.mount('#app');
