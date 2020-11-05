43c1093abe37d9ff7661a42bc712eb1d57dfe859
Lime5005 <zlp5005@gmail.com>
2020-11-05 02:08:48 +0100

    ErrorAlert component

--- learn-components/src/components/ManageGoals.vue
+++ learn-components/src/components/ManageGoals.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text">
+    <input type="text" ref="goal">
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is Invalid!</h2>
+      <p>Please enter at least a few character</p>
+      <button @click="errorConfirm">OK</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+// Use ErrorAlert if inputIsInvalid is true
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: {
+    ErrorAlert
+  },
+  data() {
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      // this.$refs NOT $ref
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        //alert('Input should not be empty!');
+        this.inputIsInvalid = true;
+      }
+    },
+    errorConfirm() {
+      this.inputIsInvalid = false;
+    }
+  }
+}
+</script>
