ffa8b898af2e94dc9318234feb991c1a9a9bba39
Lime5005 <zlp5005@gmail.com>
2020-11-11 23:18:54 +0100

    Another navigation guard: beforeRouteLeave:用于提醒用户保存资料

--- single-page-project/src/components/users/UsersList.vue
+++ single-page-project/src/components/users/UsersList.vue
@@ -1,6 +1,7 @@
 <template>
   <!--<button @click="confirm">Go Back!</button>-->
   <router-link to="/teams">Back</router-link>
+  <button @click="savedData">Confirm to leave</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -14,10 +15,18 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      savedInput: false
+    }
+  },
   methods: {
     confirm() {
       //this.$router.push('/teams');
       //this.$router.back() or forward()
+    },
+    savedData() {
+      this.savedInput = true;
     }
   },
   beforeRouteEnter(to, from, next) {
@@ -27,6 +36,18 @@ export default {
       //层层把关,指哪打哪{ name: 'team-members', params: { teamId: 't2' } }
       //alert('ok')
     );
+  },
+  // 可用于提醒用户保存输入数据：
+  beforeRouteLeave(to, from, next) {
+    if(this.savedInput) {
+      next()
+    } else {
+      const saved = confirm('Are you sure? You got some unsaved changes!')
+      // if(saved) {
+      //   next()
+      // } // next() will be executed if saved === true: 
+      next(saved)
+    }
   }
 };
 </script>
