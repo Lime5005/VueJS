7e0bf8c9197ae7ce812cb3aa8f404b625d8801c4
Lime5005 <zlp5005@gmail.com>
2020-11-14 02:36:33 +0100

    import createStore from vuex

--- learn-vuex-project/src/main.js
+++ learn-vuex-project/src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue';
-import { createStore } from 'vue';
+import { createStore } from 'vuex';
 /* one App can have only one store, but more than one state */
 import App from './App.vue';
 
-const app = createApp(App);
 const store = createStore({
     state() {
         return {
@@ -11,5 +10,7 @@ const store = createStore({
         }
     }
 })
+
+const app = createApp(App);
 app.use(store);
 app.mount('#app');
