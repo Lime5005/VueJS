60385bad8bfb85ebfa16b30870c78bfb3a9ec448
Lime5005 <zlp5005@gmail.com>
2020-11-14 23:03:44 +0100

    ...mapActions, access to all the actions methods

--- learn-vuex-project/src/components/modifyCounter.vue
+++ learn-vuex-project/src/components/modifyCounter.vue
@@ -1,21 +1,24 @@
 <template>
-  <button @click="addTen">Add 10</button>
+  <button @click="addTen({value:10})">Add 10</button>
+  <button @click="increment">Add 1</button>
 </template>
 
 <script>
+import { mapActions} from 'vuex';
 export default {
   methods: {
-    addTen() {
-      //this.$store.commit('addTen', {value: 10}); or:
-/*       this.$store.commit({
-        type: 'addTen',
-        value: 10
-      }) */
-      this.$store.dispatch({
-        type: 'addTen',
-        value: 10
-      })
-    }
+//     addTen() {
+//       //this.$store.commit('addTen', {value: 10}); or:
+// /*       this.$store.commit({
+//         type: 'addTen',
+//         value: 10
+//       }) */
+//       this.$store.dispatch({
+//         type: 'addTen',
+//         value: 10
+//       })
+//    }
+    ...mapActions(['increment', 'addTen'])
   }
 }
 </script>
