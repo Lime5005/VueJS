a76d723e8796551145b0329095f204e0b41418bd
Lime5005 <zlp5005@gmail.com>
2020-11-14 17:49:16 +0100

    Passing data between getters methods: passing through parameters-> getters

--- learn-vuex-project/src/main.js
+++ learn-vuex-project/src/main.js
@@ -20,7 +20,18 @@ const store = createStore({
     getters: {
         finalCounter(state) {
             return state.counter * 5;
+        },
+        normalizedCounter(_, getters) {
+            const finalCounter = getters.finalCounter;
+            if (finalCounter < 0) {
+                return 0;
+            }
+            if (finalCounter > 100) {
+                return 100;
+            }
+            return finalCounter;
         }
+
     }
 })
 
