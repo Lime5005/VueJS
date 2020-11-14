b42aa6ddf9cc4afec4877ab42b0d41e40a66d87c
Lime5005 <zlp5005@gmail.com>
2020-11-14 15:42:37 +0100

    mutations: control the changes to the state data

--- learn-vuex-project/src/App.vue
+++ learn-vuex-project/src/App.vue
@@ -2,21 +2,24 @@
   <base-container title="Vuex">
     <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
+    <modify-counter></modify-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import theCounter from './components/theCounter.vue';
+import modifyCounter from './components/modifyCounter.vue';
 
 export default {
   components: {
     BaseContainer,
-    theCounter
+    theCounter,
+    modifyCounter
   },
   methods: {
     addOne() {
-      this.$store.state.counter++;
+      this.$store.commit('increment');
     }
   }
 };
