76b2574b25821eecd30f03c24558cebe5a5b370e
Lime5005 <zlp5005@gmail.com>
2020-11-16 22:27:32 +0100

    show coach details

--- find-a-coach-project/src/pages/coaches/coachDetail.vue
+++ find-a-coach-project/src/pages/coaches/coachDetail.vue
@@ -2,5 +2,5 @@
   Details of the coach:
   <router-view></router-view>
     <!-- if there is a child route, you have to add the router-view on the parent -->
-    <router-link to="/contact">Contact</router-link>
+    <router-link to="contact">Contact</router-link>
 </template>
--- find-a-coach-project/src/pages/coaches/coachesList.vue
+++ find-a-coach-project/src/pages/coaches/coachesList.vue
@@ -3,13 +3,23 @@
     Filter
   </section>
   <section>
-    <div class="control">
+    <div class="controls">
       <button>Refresh</button>
       <router-link to="/register">Register as A Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
+<!--       <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
+      </li> -->
+      <li>
+        <coach-item v-for="coach in filteredCoaches" 
+        :key="coach"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas">
+        </coach-item>
       </li>
     </ul>
     <h3 v-else>No coaches found.</h3>
@@ -17,8 +27,12 @@
 </template>
 
 <script>
+import coachItem from './coachItem.vue';
 
 export default {
+  components: {
+    coachItem
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches']
@@ -29,3 +43,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
