aaa114f17605e1bea929c21fa9045102ed3bd26f
Lime5005 <zlp5005@gmail.com>
2020-11-16 01:51:25 +0100

    initial commit: wired up the components and routes

---
+++ find-a-coach-project/src/router.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import coachDetail from './pages/coaches/coachDetail.vue';
+import coachesList from './pages/coaches/coachesList.vue';
+import registration from './pages/coaches/registration.vue';
+
+import contact from './pages/requests/contact.vue';
+import reqReceived from './pages/requests/reqReceived.vue';
+
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: coachesList },
+        {
+            path: '/coaches/:id',
+            component: coachDetail,
+            children: [
+                { path: '/contact', component: contact }
+            ]
+        },
+        { path: '/register', component: registration },
+        { path: '/requests', component: reqReceived },
+        { path: '/:notFound(.*)', component: NotFound },
+
+    ]
+});
+
+export default router;
