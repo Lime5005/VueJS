18610dc31239b85e5e7db3938e42dcf6c043cbcc
Lime5005 <zlp5005@gmail.com>
2020-11-17 22:53:00 +0100

    register form CSS completed

--- find-a-coach-project/src/pages/coaches/registration.vue
+++ find-a-coach-project/src/pages/coaches/registration.vue
@@ -1,3 +1,18 @@
 <template>
-  <h2>Register</h2>
+  <section>
+    <base-card>
+    <h2>Register as A Coach now!!!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+
 </template>
+
+<script>
+import coachForm from './coachForm.vue'
+export default {
+  components: {
+    coachForm
+  }
+}
+</script>
