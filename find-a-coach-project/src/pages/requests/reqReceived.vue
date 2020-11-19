bdc22035555dc14f611ab587c4894ec7256b5b65
Lime5005 <zlp5005@gmail.com>
2020-11-19 22:55:06 +0100

    send message to request page

--- find-a-coach-project/src/pages/requests/reqReceived.vue
+++ find-a-coach-project/src/pages/requests/reqReceived.vue
@@ -1,3 +1,47 @@
 <template>
-  Request received
+  <section>
+      <base-card>
+        <header>
+          <h2>Requests received:</h2>
+        </header>
+        <ul v-if="hasRequest">
+          <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message"></request-item>
+        </ul>
+        <h3 v-else>You haven't received any request yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import requestItem from './requestItem.vue'
+export default {
+  components: {
+    requestItem
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests']
+    },
+    hasRequest() {
+      return this.$store.getters['requests/hasRequest']
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
