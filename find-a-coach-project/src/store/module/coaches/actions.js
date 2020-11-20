087d27fdcb2315bfa116f32f5d220c0c6ce241ab
Lime5005 <zlp5005@gmail.com>
2020-11-20 22:57:33 +0100

    errorHandling using a baseDialog component(ui)

--- find-a-coach-project/src/store/module/coaches/actions.js
+++ find-a-coach-project/src/store/module/coaches/actions.js
@@ -31,7 +31,8 @@ export default {
         const responseData = await response.json()
 
         if (!response.ok) {
-            //
+            const err = new Error(responseData.message || 'Failed to fetch!')
+            throw err
         }
         // transferm the data into an array:
 
