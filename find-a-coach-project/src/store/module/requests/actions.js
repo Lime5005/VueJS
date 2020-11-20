035050527574711e432cb921be1d2b2494dadb62
Lime5005 <zlp5005@gmail.com>
2020-11-20 23:14:29 +0100

    send request to server: firebase

--- find-a-coach-project/src/store/module/requests/actions.js
+++ find-a-coach-project/src/store/module/requests/actions.js
@@ -1,11 +1,26 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
+            //id: new Date().toISOString(),
+            //coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         }
+        const response = await fetch(`https://vue-http-project-960b4.firebaseio.com/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        })
+
+        const responseData = await response.json()
+        console.log(responseData);
+
+        if (!response.ok) {
+            const err = new Error(responseData.message || 'Failed to send request')
+            throw err
+        }
+        //firebase will make an id automatically called name:
+        newRequest.id = responseData.name
+        newRequest.coachId = payload.coachId
 
         context.commit('addRequest', newRequest)
     }
