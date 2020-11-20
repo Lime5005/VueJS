a14cd079c7e004c2ecde7977e695fafc79a4e54a
Lime5005 <zlp5005@gmail.com>
2020-11-20 21:20:54 +0100

    data stored in the firebase server

--- find-a-coach-project/src/store/module/coaches/actions.js
+++ find-a-coach-project/src/store/module/coaches/actions.js
@@ -1,14 +1,28 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId
         const coachData = {
-            id: context.rootGetters.userId,
-            firstName: data.firstname,
-            lastName: data.lastname,
-            description: data.description,
-            hourlyRate: data.rate,
-            areas: data.areas
-        }
+                firstName: data.firstname,
+                lastName: data.lastname,
+                description: data.description,
+                hourlyRate: data.rate,
+                areas: data.areas
+            }
+            //send a (by default)get request:, with an object, is the data we will send to register:
+        const response = await fetch(`https://vue-http-project-960b4.firebaseio.com/coaches/${userId}.json`, {
+            //PUT：如果存在就更新，不存在就新建：（而POST是不停的新建）
+            method: 'PUT',
+            body: JSON.stringify(coachData)
 
-        context.commit('registerCoach', coachData)
+        })
+
+        //const responseData = await response.json()
+        if (!response.ok) {
+            //error
+        }
+        context.commit('registerCoach', {
+            ...coachData, //copy all the coachData
+            id: userId
+        })
     }
 }
