c72659e9c7b962becabd14077f9fdb5edd971b25
Lime5005 <zlp5005@gmail.com>
2020-11-22 00:43:07 +0100

    use base-spinner for better UX in error handling

--- find-a-coach-project/src/pages/auth/userAuth.vue
+++ find-a-coach-project/src/pages/auth/userAuth.vue
@@ -1,19 +1,27 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">E-mail</label>
-        <input type="email" id="email" v-model.trim="email">
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password">
-      </div>
-      <p v-if="!formIsValid">Please enter a valid email and password (at least 6 characters).</p>
-      <base-button>{{ loginBtnCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="swithAuthMode">{{ switchBtnCaption }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      {{ error }}
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-mail</label>
+          <input type="email" id="email" v-model.trim="email">
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password">
+        </div>
+        <p v-if="!formIsValid">Please enter a valid email and password (at least 6 characters).</p>
+        <base-button>{{ loginBtnCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="swithAuthMode">{{ switchBtnCaption }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -23,7 +31,9 @@ export default {
       email: '',
       password: '',
       formIsValid: true,
-      mode: 'login'
+      mode: 'login',
+      isLoading: false,
+      error: null
     }
   },
   computed: {
@@ -43,20 +53,29 @@ export default {
     }
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       if(this.email === '' || !this.email.includes('@') || this.password.length < 6) {
         this.formIsValid = false
         return
       }
+
+      this.isLoading = true
       //send http request
-      if(this.mode === 'login') {
-        //
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password
-        })//The auth module is not namespaced.
+
+      try {
+        if(this.mode === 'login') {
+          //
+        } else {
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password
+          })//The auth module is not namespaced.
+        }
+      } catch(err) {
+        this.error = err.message || 'Failed to Sign-Up, try again later'
       }
+
+      this.isLoading = false
     },
     swithAuthMode() {
       if(this.mode === 'login'){
@@ -64,6 +83,9 @@ export default {
       } else {
         this.mode = 'login'
       }
+    },
+    handleError() {
+      this.error = null
     }
   }
 }
--- find-a-coach-project/src/store/module/auth/actions.js
+++ find-a-coach-project/src/store/module/auth/actions.js
@@ -1,7 +1,7 @@
 export default {
     async signup(context, payload) {
         //http POST request to sign up a new user:
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAeRlfuJm9r8zg67oYVzb_9XHVleILlhJQ', {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=', {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -13,7 +13,7 @@ export default {
 
         if (!response.ok) {
             console.log(responseData);
-            const err = new Error(responseData.message || 'Failed to sign up.')
+            const err = new Error(responseData.message || 'Failed to Sign-Up, try again later.')
             throw err
         }
 
