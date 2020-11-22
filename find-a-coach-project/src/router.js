9d3365f42326d573a9accbd92482214283874e6f
Lime5005 <zlp5005@gmail.com>
2020-11-22 23:56:31 +0100

    navigation guard for register and requests, and auth pages

--- find-a-coach-project/src/router.js
+++ find-a-coach-project/src/router.js
@@ -11,6 +11,8 @@ import NotFound from './pages/NotFound.vue';
 
 import userAuth from './pages/auth/userAuth.vue';
 
+import store from './store/index.js'
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -24,12 +26,24 @@ const router = createRouter({
                 { path: 'contact', component: contact }
             ]
         },
-        { path: '/register', component: registration },
-        { path: '/requests', component: reqReceived },
-        { path: '/auth', component: userAuth },
+        { path: '/register', component: registration, meta: { requiresAuth: true } }, //如果没登陆不可见
+        { path: '/requests', component: reqReceived, meta: { requiresAuth: true } }, //如果没登陆不可见
+        { path: '/auth', component: userAuth, meta: { requiresNotAuth: true } }, //如果已经登陆则不见
         { path: '/:notFound(.*)', component: NotFound },
 
     ]
 });
+
+//Add the global guard to every navigation: add a meta data to protect it
+router.beforeEach(function(to, _, next) { //(to, from, next)
+    //to.meta.requiresAuth 我要去的页面有auth要求，并且我没有auth
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth') //next() 程序继续； next(false)程序终止；
+    } else if (to.meta.requiresNotAuth && store.getters.isAuthenticated) {
+        next('/coaches')
+    } else {
+        next()
+    }
+})
 
 export default router;
