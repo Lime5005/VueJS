ce700f9fd2d32ead307f5abb968c975c5fd92d2e
Lime5005 <zlp5005@gmail.com>
2020-11-25 00:13:25 +0100

    ref & reactive: composition API

---
+++ assignment7/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <h2>My course Goal</h2>
+
+  <h3>{{ goalAndYear.goal}}</h3>
+  <h3>{{ goalAndYear.year }}</h3>
+
+  <button @click="showGoal">Toggle Goal</button>
+
+  
+  <button @click="growYear">Year for next year</button>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  setup() {
+    //const goal = ref('Find a job in IT')
+    const goalAndYear = reactive({
+      goal: 'Find a job in IT',
+      year: 2020
+    })
+/*     function showGoal(){
+      if(goal.value !== '') {
+        goal.value = ''
+      } else {
+        goal.value = 'Find a job in IT'
+      }
+    } */
+  function showGoal() {
+    if(goalAndYear.goal !== '') {
+      goalAndYear.goal = ''
+    } else {
+      goalAndYear.goal = 'Find a job in IT'
+    }
+  }
+
+  function growYear() {
+    goalAndYear.year ++ 
+  }
+
+    return {goalAndYear: goalAndYear, showGoal: showGoal, growYear: growYear}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
