e40af87782580c2dd41807b636dada85f6a11145
Lime5005 <zlp5005@gmail.com>
2020-11-27 18:03:57 +0100

    lifecycle Hooks in composition API

--- assignment7/src/components/userList.vue
+++ assignment7/src/components/userList.vue
@@ -6,12 +6,23 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue'
+import { computed, 
+inject, 
+onBeforeMount,
+onUpdated
+} from 'vue'
 export default {
   props: ['firstName', 'lastName'],
   setup(props, context) {
     const userName = computed(() => {
       return props.firstName + ' ' + props.lastName
+    })
+    onBeforeMount(() => {
+      console.log('Before Mount');
+    })
+
+    onUpdated(() => {
+      console.log('On updated');
     })
     console.log(context);//attrs, emit,...if need emit:
     //context.emit('save-data', 1) === this.$emit('save-data', 1)
